b03255caa133067bb19083a3f55a9ec95e514767
Andres Ramirez Morales <stormchildremix@gmail.com>
2023-11-02 11:24:40 -0600

    add functions

---
+++ feyncalc/LoopIntegrals/inc/ChromoDipoleTopQuark.h
@@ -0,0 +1,18 @@
+//ChromoDipoleTopQuark includes
+#ifndef CHROMODIPOLETOPQUARK_H
+#define CHROMODIPOLETOPQUARK_H
+
+#include <string>
+#include <vector>
+
+class ChromoDipoleTopQuark{
+
+  public:
+  ChromoDipoleTopQuark();
+  virtual ~ChromoDipoleTopQuark();
+  virtual double execute();
+
+}
+
+
+#endif //> !CHROMODIPOLETOPQUARK_H
---
+++ feyncalc/LoopIntegrals/src/ChromoDipoleTopQuark.cxx
@@ -0,0 +1,24 @@
+//ChromoDipole class includes
+#ifndef CHROMODIPOLETOPQUARK_CXX
+#define CHROMODIPOLETOPQUARK_CXX
+
+#include "ChromoDipoleTopQuark.h"
+
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <stdio.h>
+//#include <complex>
+ChromoDipoleTopQuark::ChromoDipoleTopQuark()
+{
+}
+
+ChromoDipoleTopQuark::~ChromoDipoleTopQuark(){}
+
+double ChromoDipoleTopQuark::execute(){
+  return 0.;
+}
+
+
+#endif
